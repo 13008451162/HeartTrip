070d100f8a35cea94fde1dbd843727b2c6247459
lukecc0 <s1793026645@163.com>
2023-12-20 23:19:36 +0800

    初始版本

---
+++ useronboarding/src/main/java/com/xupt3g/hearttrip/useronboarding/login/LoginUiFragment.java
@@ -0,0 +1,27 @@
+package com.xupt3g.hearttrip.useronboarding.login;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * 项目名: HeartTrip
+ * 文件名: LoginUiFragment
+ *
+ * @author: lukecc0
+ * @data:2023/12/20 下午7:55
+ * @about: TODO 登录界面的Ui展示
+ */
+
+public class LoginUiFragment extends Fragment {
+
+
+    /**
+     * 检测用户登录
+     * @param user 用户账号
+     * @param password 密码
+     * @return boolean 是否登录成功
+     */
+    private boolean loginStatus(String user,String password){
+
+        return true;
+    }
+}
